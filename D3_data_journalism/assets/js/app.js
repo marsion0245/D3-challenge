9affabbc4d46e72a965f8a8c928d9b71aa716905
Martin Hrbac <martin.hrbac@siemens.com>
2019-10-11 23:24:51 -0400

    added instructions

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,16 @@
-// @TODO: YOUR CODE HERE!
+
+/*
+D3-challenge HW by martin hrbac
+*/
+
+// IIFE
+(function(){
+
+	d3.csv("/data/cities.csv").then(function(data) {
+	  console.log(data[0]);
+	});
+	
+	
+
+})();
+
